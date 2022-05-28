c165a009f6338f3bbf7f2e02356375fb1e9a4df8
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-05-29 04:27:41 +0900

    Added function : sign_in_view

--- django/sns/user/views.py
+++ django/sns/user/views.py
@@ -1,6 +1,8 @@
 # user의 urls.py에 연결 할 user앱의 views.py 작성하기
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import UserModel
+
 
 
 def sign_up_view(request):
@@ -22,5 +24,19 @@ def sign_up_view(request):
             new_user.save() # 데이터베이스에도 저장을 해 줌.
         return redirect('/sign-in')
 
+
+
 def sign_in_view(request):
-    return render(request, 'user/signin.html')
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        me = UserModel.objects.get(username=username)  # 사용자 불러오기
+        if me.password == password:  # 저장된 사용자의 패스워드와 입력받은 패스워드 비교
+            request.session['user'] = me.username  # 세션에 사용자 이름 저장
+            return HttpResponse("로그인 성공!")
+        else: 
+            return redirect('/sign-in')
+    elif request.method == 'GET':
+        return render(request, 'user/signin.html')
+
