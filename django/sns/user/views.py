808947b3f772e181905699c2834cbe3d1557f674
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-05-29 13:41:33 +0900

    Added function : sign_up_view : username 중복확인

--- django/sns/user/views.py
+++ django/sns/user/views.py
@@ -16,13 +16,19 @@ def sign_up_view(request):
 
         if password != password2: # 입력되는 password와 재확인하는 password2 값이 다르다면,
             return render(request, 'user/signup.html')
-        else:
-            new_user = UserModel() # new_user 변수명으로 UserModel(클래스)를 가져옴 > 테이블 이름은 my_user로, 데이터베이스 정보는 클래스 Meta 에 적어줌.
-            new_user.username = username 
-            new_user.password = password 
-            new_user.bio = bio # 지금까지의 코드에서 UserModel은 admin에'만' 저장이 되어서
-            new_user.save() # 데이터베이스에도 저장을 해 줌.
-        return redirect('/sign-in')
+        else: 
+            me = UserModel.objects.filter(username=username)
+            if me: # UserModel에서 불러온 사용자와 입력한 사용자가 일치할 경우,
+                print('해당 아이디가 이미 존재합니다')
+                return render(request, 'user/signup.html')
+            else: 
+                new_user = UserModel() # new_user 변수명으로 UserModel(클래스)를 가져옴 > 테이블 이름은 my_user로, 데이터베이스 정보는 클래스 Meta 에 적어줌.
+                new_user.username = username 
+                new_user.password = password 
+                new_user.bio = bio # 지금까지의 코드에서 UserModel은 admin에'만' 저장이 되어서
+                new_user.save() # 데이터베이스에도 저장을 해 줌.
+                print('신규 가입 성공')
+            return redirect('/sign-in')
 
 
 
