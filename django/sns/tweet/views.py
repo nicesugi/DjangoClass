5ae36693601691f80581af7e83cb25f676a4f344
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-05-29 15:57:18 +0900

    Add tweet.views.py

--- django/sns/tweet/views.py
+++ django/sns/tweet/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+def home(request):
+    user = request.user.is_authenticated
+    if user:
+        return redirect('/tweet')
+    else:
+        return redirect('sign-in')
+
+def tweet(request):
+    if request.method =='GET':
+        return render(request, 'tweet/home.html')
+    
